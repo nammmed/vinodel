2086be62414a09fc1f5ee640e7e625e761f7c586
Nammmed <bilenkoyar@gmail.com>
2024-12-03 14:44:24 +0300

    autoload

--- public/server/index.php
+++ public/server/index.php
@@ -23,17 +23,11 @@ session_set_cookie_params([
     'samesite' => 'None'
 ]);
 
-require_once 'config/config.php';
+// Подключаем конфигурацию
+require_once __DIR__ . '/config/config.php';
 
-// Подключение автозагрузки, если вы настроили ее, или вручную подключите нужные файлы
-require_once 'models/BaseModel.php';
-require_once 'models/User.php';
-require_once 'models/Batch.php';
-require_once 'models/Grape.php';
-require_once 'controllers/UserController.php';
-require_once 'controllers/BaseController.php';
-require_once 'controllers/BatchController.php';
-require_once 'controllers/GrapeController.php';
-// Подключите остальные модели и контроллеры по мере необходимости
+// Подключаем автозагрузчик
+require_once __DIR__ . '/autoload.php';
 
-require_once 'routes/routes.php';
+// Подключаем маршруты
+require_once __DIR__ . '/routes/routes.php';
